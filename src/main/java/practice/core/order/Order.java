87ee2dba660c4fdd10a1b8e496e95cbd1301f830
seunghyun711 <seunghyun711@gmail.com>
2023-07-07 00:18:38 +0900

    Order 엔티티 클래스 새성

---
+++ src/main/java/practice/core/order/Order.java
@@ -0,0 +1,60 @@
+package practice.core.order;
+
+/**
+ * <주문 엔티티>
+ */
+public class Order {
+    private Long memberId; // 주문한 회원의 id
+    private String itemName; // 주문 상품 이름
+    private int itemPrice; // 상품 가격
+    private int discountPrice;  // 할인 가격
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(int discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
