1be665d0e161864f0bfe7e1b7362f787433190e2
seunghyun711 <seunghyun711@gmail.com>
2023-07-11 00:31:00 +0900

    AppConfig 스프링으로 전환

--- src/main/java/practice/core/AppConfig.java
+++ src/main/java/practice/core/AppConfig.java
@@ -1,5 +1,7 @@
 package practice.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import practice.core.discount.DiscountPolicy;
 import practice.core.discount.FixDiscountPolicy;
 import practice.core.discount.RateDiscountPolicy;
@@ -10,19 +12,25 @@ import practice.core.member.MemoryMemberRepository;
 import practice.core.order.OrderService;
 import practice.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
