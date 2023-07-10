1d034034b0241305909c7a6b0e027e572113d5d4
seunghyun711 <seunghyun711@gmail.com>
2023-07-10 16:14:53 +0900

    테스트 코드 수정(생성자 주입)

--- src/test/java/practice/core/member/MemberServiceTest.java
+++ src/test/java/practice/core/member/MemberServiceTest.java
@@ -1,15 +1,22 @@
 package practice.core.member;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import practice.core.AppConfig;
 
 /**
  * <회원 도메인 테스트>
  */
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
 
+    @BeforeEach // 각 테스트 실행 전 호출되도록 하는 애너테이션
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
     @Test
     @DisplayName("회원 가입 테스트")
     void join(){
--- src/test/java/practice/core/order/OrderServiceTest.java
+++ src/test/java/practice/core/order/OrderServiceTest.java
@@ -1,8 +1,10 @@
 package practice.core.order;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import practice.core.AppConfig;
 import practice.core.member.Grade;
 import practice.core.member.Member;
 import practice.core.member.MemberService;
@@ -12,8 +14,15 @@ import practice.core.member.MemberServiceImpl;
  * <주문 및 할인 정책 테스트>
  */
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach // 각 테스트 실행 전 호출되도록 하는 애너테이션
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     @DisplayName("주문 및 할인 정책 테스트")
