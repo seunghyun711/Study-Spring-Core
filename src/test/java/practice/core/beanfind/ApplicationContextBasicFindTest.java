0c09b9645df4ec90c9f3015f15eefef40077de11
seunghyun711 <seunghyun711@gmail.com>
2023-07-12 23:37:07 +0900

    빈 이름으로 스프링 빈 조회 테스틋

---
+++ src/test/java/practice/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,23 @@
+package practice.core.beanfind;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import practice.core.AppConfig;
+import practice.core.member.MemberService;
+import practice.core.member.MemberServiceImpl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+}
