b87a368b3ae75ca05c2f9b6e1c47b4fd77da85d1
seunghyun711 <seunghyun711@gmail.com>
2023-07-13 16:14:03 +0900

    NoSuchBeanDefinitonException 테스트

--- src/test/java/practice/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/practice/core/beanfind/ApplicationContextBasicFindTest.java
@@ -3,6 +3,7 @@ package practice.core.beanfind;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import practice.core.AppConfig;
 import practice.core.member.MemberService;
@@ -33,5 +34,14 @@ public class ApplicationContextBasicFindTest {
     void findBeanByName2(){
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX(){
+//        MemberService memberService = ac.getBean("Unknown", MemberService.class);
+//        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+        org.junit.jupiter.api.Assertions.assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("Unknown", MemberService.class));
     }
 }
