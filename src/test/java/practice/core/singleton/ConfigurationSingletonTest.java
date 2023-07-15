4ba54a41898fbc975ca078188610cd6aea3cf1cf
seunghyun711 <seunghyun711@gmail.com>
2023-07-15 13:14:56 +0900

    @Configuration & 싱글톤 테스트

--- src/test/java/practice/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/practice/core/singleton/ConfigurationSingletonTest.java
@@ -26,4 +26,14 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass()); // 클래스 타입 확인
+    }
 }
