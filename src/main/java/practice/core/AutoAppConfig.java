184df4e19638a9adfc95773f6521d97edb09dad7
seunghyun711 <seunghyun711@gmail.com>
2023-07-17 21:21:42 +0900

    컴포넌트 스캔 탐색 위치 지정

--- src/main/java/practice/core/AutoAppConfig.java
+++ src/main/java/practice/core/AutoAppConfig.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages ="practice.core.member", //member만 컴포넌트 스캔의 대상이 된다.
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        // @Configuration이 붙은 클래스는 컴포넌트 스캔 대상에서 제외
 )
 public class AutoAppConfig {
 }
