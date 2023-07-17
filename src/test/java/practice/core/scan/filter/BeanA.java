849d8357e873509c45089d93f65cf07a80dba5c4
seunghyun711 <seunghyun711@gmail.com>
2023-07-17 21:30:31 +0900

    컴포넌트 스캔 대상에서 추가 및 제외할 클래스

---
+++ src/test/java/practice/core/scan/filter/BeanA.java
@@ -0,0 +1,6 @@
+package practice.core.scan.filter;
+
+// 컴포넌트 스캔 대상에 추가할 클래스
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/test/java/practice/core/scan/filter/BeanB.java
@@ -0,0 +1,6 @@
+package practice.core.scan.filter;
+
+// 컴포넌트 스캔 대상에서 제외할 클래스
+@MyExcludeComponent
+public class BeanB {
+}
