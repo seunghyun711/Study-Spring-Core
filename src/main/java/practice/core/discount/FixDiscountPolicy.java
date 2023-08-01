cc8299fe9da9a8689448f0ef99942fc69b633acd
seunghyun711 <seunghyun711@gmail.com>
2023-08-01 14:19:24 +0900

    조회할 빈이 2개 이상인 경우 해결방법 3 -  @Primary 사용

--- src/main/java/practice/core/discount/FixDiscountPolicy.java
+++ src/main/java/practice/core/discount/FixDiscountPolicy.java
@@ -10,7 +10,6 @@ import practice.core.member.Member;
  * 회원등급이 VIP인 회원에 대해 1000원 할인
  */
 @Component
-@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
     private final int discountFixAmount = 1000; // 할인 금액 : 1000원
     @Override
--- src/main/java/practice/core/discount/RateDiscountPolicy.java
+++ src/main/java/practice/core/discount/RateDiscountPolicy.java
@@ -1,6 +1,7 @@
 package practice.core.discount;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import practice.core.member.Grade;
 import practice.core.member.Member;
@@ -10,7 +11,7 @@ import practice.core.member.Member;
  * VIP인 고객의 주문 금액의 10% 할인 적용
  */
 @Component
-@Qualifier("mainDiscountPolicy")
+@Primary // 조회할 빈이 2개 이상인 경우 해결방법 3 -  @Primary 사용
 public class RateDiscountPolicy implements DiscountPolicy{
     private int discountPercent = 10; // 10% 할인
 
