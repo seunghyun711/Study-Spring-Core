b1ebe6d092cef7db627cdae12e98696f95fe835f
seunghyun711 <seunghyun711@gmail.com>
2023-08-22 13:08:18 +0900

    BeanLifeCycle test

---
+++ src/test/java/practice/core/lifecycle/BeanLifeCycle.java
@@ -0,0 +1,27 @@
+package practice.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycle {
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close(); // 스프링 컨테이너 종료
+
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://lifecycle.com");
+            return networkClient;
+        }
+    }
+}
