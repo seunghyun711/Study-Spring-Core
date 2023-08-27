4581bdd0bf145c837c9da5313c6d3cafec346a41
seunghyun711 <seunghyun711@gmail.com>
2023-08-27 20:18:02 +0900

    빈 스코프 - 싱글톤에서 프로토타입 빈 사용

--- src/test/java/practice/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/practice/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package practice.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -9,6 +10,20 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void singletonClientUsePrototype(){ // 싱그론 빈에서 프로토타입 빈 사용 테스트
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(2);
+    }
+
     @Test
     void prototypeFind() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
@@ -20,6 +35,22 @@ public class SingletonWithPrototypeTest1 {
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
         prototypeBean2.addCount();
         Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Scope("singleton") // 싱글톤 빈 스코프
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean) {// PrototypeBean 의존관계 주입
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic(){
+            prototypeBean.addCount();
+            int count = prototypeBean.count;
+            return count;
+        }
     }
 
     @Scope("prototype") // 프로토타입 빈 스코프
