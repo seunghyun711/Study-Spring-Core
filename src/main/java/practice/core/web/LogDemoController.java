5203a9c6d98a39176af74f1b17cd214229ae4243
seunghyun711 <seunghyun711@gmail.com>
2023-09-19 14:39:13 +0900

    빈 스코프 - 웹 스코프 사용(Provider 방식)

--- src/main/java/practice/core/web/LogDemoController.java
+++ src/main/java/practice/core/web/LogDemoController.java
@@ -1,6 +1,7 @@
 package practice.core.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +13,13 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController { // 로거가 작동하는지 확인하는 테스트용 컨트롤러
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) { // HttpServletRequest를 통해 요청 URL(http://localhost:8080/log-demo)을 받는다.
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL); // requestURL 값을 myLogger에 저장
 
         myLogger.log("controller test"); // 로그를 남긴다.
--- src/main/java/practice/core/web/LogDemoService.java
+++ src/main/java/practice/core/web/LogDemoService.java
@@ -1,15 +1,17 @@
 package practice.core.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import practice.core.common.MyLogger;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService { // 서비스 계층
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
